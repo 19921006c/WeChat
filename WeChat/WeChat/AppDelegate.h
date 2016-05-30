94027dd64cf5b1fb87927ef1050c1d24a34d5aec
zhangzhihao <zhangzhihao@xywy.com>
2016-05-30 11:05:11 +0800

    创建微信项目

---
+++ WeChat/WeChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeChat
+//
+//  Created by J on 16/5/30.
+//  Copyright © 2016年 J. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
